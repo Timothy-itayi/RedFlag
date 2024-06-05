ca721bb70f9b6c61fb980d8b9ac2912b7789df01
Tim Itayi <timitayi@Timothys-Mac.local>
2024-06-05 17:17:35 +1200

    complete the manufacturer page

--- greenflag/src/pages/manufacturer.tsx
+++ greenflag/src/pages/manufacturer.tsx
@@ -13,39 +13,38 @@ const  Manufacturers = () =>  {
 export default Manufacturers
 
 
-const Lotus = () => {
+const Renualt = () => {
   return (
     <div>
-       <p className="font-bold text-4xl text-white">Lotus</p>
+       <p className="font-bold text-4xl text-white">Renualt</p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-      Seventy-five races and 20 wins does not sound that impressive in the present company. But the Lotus 72 has an ace up its sleeve. It won three Constructors’ Championships and two Drivers’ Championships in six seasons of racing and its 20 victories are spread out over five seasons, not just one. The Lotus 72 is a special racing car.
-      </p>
+      Renault, a French automaker, has been in Formula One since 1977 as both a team owner and engine supplier. It introduced the turbo engine and won multiple titles with Williams and Benetton. After rebranding Benetton as Renault in 2000, it won championships in 2005 and 2006. Renault powered Red Bull's victories from 2010 to 2013 and rebranded its team as Alpine in 2021. Renault boasts two constructors' and drivers' titles as a team and 12 constructors' and 11 drivers' titles as an engine supplier.
+  </p>
     </div>
   );
 };
 
-const Mclaren = () => {
+const Mercedes = () => {
   return (
     <div>
-   <p className="font-bold text-4xl text-white">Mclaren</p>
+   <p className="font-bold text-4xl text-white">Mercedes</p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-      The McLaren MP4/4 is statistically for now at least the best F1 car of all time. In the 1988 F1 season, in the hands of Alain Prost and Ayrton Senna, the MP4/4 won 15 of the 16 races, only missing out on the complete set at the Italian Grand Prix thanks to Senna's famous collision with Jean-Louis Schlesser. Just one month after Enzo Ferrari died, even McLaren had to feel OK with losing that one win to Ferrari.
+      Mercedes-Benz, a German luxury automotive brand, active in Formula One since 1954, owns the Mercedes-AMG Petronas F1 Team. Known as the "Silver Arrows," they won titles in 1954 and 1955. Returning as an engine supplier in 1994, Mercedes found success with McLaren. Rebranding Brawn GP in 2010, Mercedes dominated from 2014 to 2021, winning seven Drivers' and eight Constructors' titles. Mercedes engines have over 200 wins, ranking second in F1 history with ten Constructors' and thirteen Drivers' Championships.
 
 
       </p>
     </div>
   );
 };
-const Redbull = () => {
+const Honda = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Redbull</p>
+      <p className="font-bold text-4xl text-white">Honda</p>
       <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+      <p className="font-normal text-base my-2 max-w-lg text-neutral-200">
+      Honda, a Japanese automaker, has been involved in Formula One since 1964 as both an engine manufacturer and team owner. After winning its first race in 1965, Honda withdrew in 1968. Returning in 1983 as an engine supplier, Honda achieved significant success with Williams and McLaren, winning multiple titles until 1992. After a hiatus, Honda partnered with Red Bull Racing, winning the 2022 title with Max Verstappen. From 2023, Honda supplied power units as Honda RBPT. Plans for a full return in 2026 with Aston Martin have been announced.
       </p>
     </div>
   );
@@ -55,41 +54,57 @@ const Ferrari = () => {
     <div>
       <p className="font-bold text-4xl text-white">Ferrari</p>
       <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A house by the river is a place of peace and tranquility. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+      <p className="font-normal text-base my-2 max-w-lg text-neutral-200">
+      Scuderia Ferrari, the racing arm of the luxury Italian automaker, is the oldest and most successful Formula One team, competing since 1950. Founded by Enzo Ferrari, it initially raced Alfa Romeo cars before building its own in 1947. Ferrari holds 16 Constructors' Championships and a record 15 Drivers' Championships. Michael Schumacher's five consecutive titles and 72 Grands Prix victories mark the team's most successful era. Their passionate fan base, known as the tifosi, and the Italian Grand Prix at Monza are iconic aspects of the team's heritage.
       </p>
     </div>
   );
 };
+const Audi = () => {
+    return (
+      <div>
+        <p className="font-bold text-4xl text-white">Audi</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-2 max-w-lg text-neutral-200">
+        German car manufacturer Audi is contracted to compete as a Formula One team and constructor beginning with the 2026 season. Audi F1 Team will be formed through the acquisition of the Sauber Group, with engines developed by Audi Formula Racing GmbH.</p>
+      </div>
+    );
+  };
 
 const cards = [
   {
     id: 1,
-    content: <Lotus />,
-    className: "md:col-span-2",
+    content: <Renualt />,
+    className: "md:col-span-1",
     thumbnail:
-      "/manufactureLogos/lotus_logo.png",
+      "/manufacturer/Renault-F1.jpg",
   },
   {
     id: 2,
-    content: <Mclaren />,
+    content: <Mercedes />,
     className: "col-span-1",
     thumbnail:
-       "/manufactureLogos/macleran_logo.jpeg",
+       "/manufacturer/mercedes.jpeg",
   },
   {
     id: 3,
-    content: <Redbull />,
+    content: <Honda/>,
     className: "col-span-1",
     thumbnail:
-      "/manufactureLogos/redbull_logo.png",
+    "/manufacturer/honda.png",
   },
   {
     id: 4,
     content: <Ferrari />,
-    className: "md:col-span-2",
+    className: "md:col-span-1",
     thumbnail:
-       "/manufactureLogos/ferrari_logo.png",
+       "/teams/fard.avif",
+  },
+  {
+    id: 5,
+    content: <Audi />,
+    className: "md:col-span-1",
+    thumbnail:
+       "/manufacturer/audi.png",
   },
 ];
