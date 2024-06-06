55fd19e6d381f29f6f3b7f7042ed356d5e366942
Tim Itayi <timitayi@Timothys-Mac.local>
2024-06-06 12:39:55 +1200

    add new drivers Champions pages

--- greenflag/src/components/ui/stick-scroll-reveal.tsx
+++ greenflag/src/components/ui/stick-scroll-reveal.tsx
@@ -64,7 +64,7 @@ export const StickyScroll = ({
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
+      className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md "
       ref={ref}
     >
       <div className="div relative flex items-start px-4">
@@ -101,7 +101,7 @@ export const StickyScroll = ({
       <div
         style={{ background: backgroundGradient }}
         className={cn(
-          "hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden",
+          "hidden lg:block h-60 w-80 rounded-md bg-black sticky top-10 overflow-hidden",
           contentClassName
         )}
       >
