bcb4953371e7dcfbed687c5c025f0fdc3fe41b98
Tim Itayi <timitayi@Timothys-Mac.local>
2024-06-11 13:27:16 +1200

    create driver cards to display latest session and position

--- greenflag/src/components/driver-components/standings.tsx
+++ greenflag/src/components/driver-components/standings.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import {HoverEffect } from '../driver-components/hover-effect';
+import { HoverEffect } from '../driver-components/hover-effect';
 import { Driver } from "../../types/driver"; // Assuming you have a type for Driver
+
+// Function to replace special characters in a string with their normal equivalents
+const replaceSpecialCharacters = (str: string) => {
+  const specialCharactersMap: { [key: string]: string } = {
+    'é': 'e',
+    'ü': 'u',
+    // Add more mappings as needed
+  };
+
+  return str.replace(/[^\w\s]/gi, (char) => specialCharactersMap[char] || '');
+};
 
 interface StandingsProps {
   drivers: Driver[];
@@ -8,23 +19,23 @@ interface StandingsProps {
 
 const Standings: React.FC<StandingsProps> = ({ drivers }) => {
 
-
   if (!drivers || drivers.length === 0) {
     // If drivers is undefined, null, or empty, return a loading indicator or an error message
     return <div>Loading...</div>;
   }
 
- // Adjust hoverEffectItems to match the DriverInfo interface
-const hoverEffectItems = drivers.map((driver) => ({
-  title: `${driver.firstName} ${driver.lastName}`,
-
-  link: `/drivers/${driver.firstName.toLowerCase()}_${driver.lastName.toLowerCase()}`,
-  image: driver.imageUrl ?? '/page0.JPG',
-  team: driver.teamName,
-  points: driver.standing ? driver.standing.points ?? 0 : 'N/A', // Check if standing exists before accessing points
-  position: driver.standing ? driver.standing.position ?? 0 : 'N/A', // Check if standing exists before accessing position
-}));
-
+  // Adjust hoverEffectItems to match the DriverInfo interface
+  const hoverEffectItems = drivers.map((driver) => ({
+    // Normalize driver name by removing special characters
+    title: `${replaceSpecialCharacters(driver.firstName)} ${replaceSpecialCharacters(driver.lastName)}`,
+    // Encode the normalized driver name for the URL
+    link: `/drivers/${encodeURIComponent(replaceSpecialCharacters(driver.firstName.toLowerCase()))}_${encodeURIComponent(replaceSpecialCharacters(driver.lastName.toLowerCase()))}`,
+    // Encode the image URL
+    image: encodeURIComponent(driver.imageUrl ?? '/page0.JPG'),
+    team: driver.teamName,
+    points: driver.standing ? driver.standing.points ?? 0 : 'N/A',
+    position: driver.standing ? driver.standing.position ?? 0 : 'N/A',
+  }));
 
   return (
     <div className="max-w-5xl mx-auto px-8">
