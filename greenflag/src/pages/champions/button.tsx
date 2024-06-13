cbd264030c8321ff54837aca1ac4e3da0e7540c2
Tim Itayi <timitayi@Timothys-Mac.local>
2024-06-13 14:23:05 +1200

    complete max ,vettel and start kimi

--- greenflag/src/pages/champions/button.tsx
+++ greenflag/src/pages/champions/button.tsx
@@ -9,7 +9,7 @@ const  Button = () =>  {
   return (
     <>
     <div className="text-center p-10 ">
-          <h1 className="text-7xl  text-white card-font flex flex-wrap justify-center "> Damon Hill</h1>
+          <h1 className="text-7xl  text-white card-font flex flex-wrap justify-center "> Jenson Button</h1>
       </div>
       <div className="h-[20rem] md:h-[20rem] [perspective:1000px] mt-2 relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
       <Tabs tabs={jenson} />
@@ -32,10 +32,158 @@ const jenson  = [
           <div className="md:mx-auto container  border-full border-2 ">
             <BlogCard
               imageUrls={[
-                "/drivers_champions/verstappen/ver00.jpeg",
+                "/drivers_champions/jenson_button/jen00.jpeg",
               ]}
               title="Early life"
-              description="Verstappen's racing career began in karts at four, and by seven, he was competing in championships. He swiftly climbed through the ranks, winning various titles including the Mini Junior championship in Belgium. He joined CRG's factory team in 2010, excelling internationally with wins in the KF3 World Cup and WSK World Series. In 2013, at just 15, he made history by winning multiple European and World Championships in karting, becoming the youngest ever to win the KZ World Championship."/>
+              description="Button's karting journey began in 1988 at Clay Pigeon Raceway, receiving a Zip go-kart as a Christmas gift in 1987. He won the British Super Prix at nine and dominated the 1991 British Cadet Kart Championship with 34 wins.utton's achievements included winning the European Super A Championship and the Ayrton Senna Memorial Cup in 1997, establishing himself as a promising talent in karting."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Formula 3",
+    value: "formula 3",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen01.avif",
+              ]}
+              title="Junior Career"
+              description="At 18, Button began his single-seater racing career under Paul Lemmens and Harald Huysman's guidance. He won the British Formula Ford Championship in 1998 with nine victories and also triumphed at the Formula Ford Festival. Despite initial hurdles securing a Formula Three seat, Button impressed Serge Saulnier during a test at Magny-Cours, earning a drive in the British Formula 3 International Series in 1999. He won three races and finished third overall, establishing himself as a rising talent in single-seater racing."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Formula 1",
+    value: "formula 1",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen02.jpeg",
+              ]}
+              title="Debut"
+              description="Button made history as Britain's youngest Formula One driver, debuting without an FIA Super Licence, which required special dispensation. He scored his first point with a sixth-place finish in Brazil and showcased promising pace against teammate Ralf Schumacher early in the season. Despite a mid-season dip in form and the announcement of Juan Pablo Montoya as his replacement, Williams retained Button's contract. He finished his debut season with a best result of fourth in Germany and eighth in the championship standings."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Benetton",
+    value: "benetton",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen03.jpeg",
+              ]}
+              title="2001"
+              description="Button joined Benetton alongside Giancarlo Fisichella, just after Renault's acquisition of the team. Despite struggling with an uncompetitive car lacking power steering and horsepower, Button managed a fifth-place finish at the German Grand Prix. His inconsistent performance led to speculation about his future in the team, with team principal Flavio Briatore expressing doubts due to Button's struggle in car setup and a perceived lavish lifestyle."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Renault",
+    value: "renault",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen04.jpeg",
+              ]}
+              title="2002"
+              description="Rebranded as Renault for 2002, the team paired Button with Jarno Trulli. Button made efforts to enhance his public image, altering his social habits and focusing more on training. Despite outscoring his teammate with stronger race performances, Button was informed by Briatore that Fernando Alonso would replace him for 2003. Button subsequently signed with BAR, impressed by their long-term vision under team principal David Richards, and finished his final season with Renault in seventh place overall."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "BAR",
+    value: "bar",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen05.jpeg",
+              ]}
+              title="2003-2005"
+              description="In 2003, Button's relationship with teammate Jacques Villeneuve started with tension, exacerbated by an incident in Australia. Despite challenges, Button improved, securing two fourth-place finishes and finishing ninth in the Drivers' Championship with 17 points. In 2004, Button focused on consistency and podiums. Button secured ten podiums, finishing third in the Drivers' Championship and contributing to BAR's second place in the Constructors' Championship."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Honda",
+    value: "honda",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen06.jpeg",
+              ]}
+              title="2006-2008"
+              description="Following Honda's buyout of BAR and rebranding as Honda, Button partnered with Rubens Barrichello under equal status. In 2006, Button achieved his first victory at the Hungarian Grand Prix and finished the season strongly. In 2007, early-season injuries and aerodynamic issues with the Honda RA107 led to poor results, ending the season 15th in the Drivers' Championship. In 2008, despite rigorous preparation, Button and Barrichello struggled with the uncompetitive RA108, earning only three points. Honda's sudden exit from Formula 1 in December 2008 left Button's future uncertain."/>
+           
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Brawn",
+    value: "brawn",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/jenson_button/jen07.jpeg",
+              ]}
+              title="World Champion "
+              description="
+Brawn GP emerged in 2009 from Honda's former team, acquired inexpensively. Jenson Button joined despite a pay cut, and the Brawn BGP 001 impressed in pre-season testing with efficient aerodynamics, a Mercedes-Benz V8 engine, and tailored slick tyres. Button dominated with six wins in the first seven races and four poles, aided by a controversial double diffuser. Despite a weaker second half, his early points secured the championship before the season's end. At the Brazilian Grand Prix, starting 14th, Button strategically finished fifth to clinch the title with a race to spare, confirmed by a podium in Abu Dhabi."/>
            
           </div>
       
--- greenflag/src/pages/champions/hamilton.tsx
+++ greenflag/src/pages/champions/hamilton.tsx
@@ -9,10 +9,10 @@ const  Hamilton = () =>  {
   return (
     <>
     <div className="text-center p-10 ">
-          <h1 className="text-7xl  text-white card-font flex flex-wrap justify-center "> Damon Hill</h1>
+          <h1 className="text-7xl  text-white card-font flex flex-wrap justify-center "> Lewis Hamilton</h1>
       </div>
       <div className="h-[20rem] md:h-[20rem] [perspective:1000px] mt-2 relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
-      <Tabs tabs={lewis} />
+      <Tabs tabs={items} />
       </div>
       </>
   );
@@ -20,7 +20,7 @@ const  Hamilton = () =>  {
 
 export default Hamilton;
 
-const lewis  = [
+const items  = [
   {
     title: "Karting",
     value: "karting",
@@ -32,11 +32,149 @@ const lewis  = [
           <div className="md:mx-auto container  border-full border-2 ">
             <BlogCard
               imageUrls={[
-                "/drivers_champions/verstappen/ver00.jpeg",
+                "/drivers_champions/hamilton/ham00.webp",
               ]}
               title="Early life"
-              description="Verstappen's racing career began in karts at four, and by seven, he was competing in championships. He swiftly climbed through the ranks, winning various titles including the Mini Junior championship in Belgium. He joined CRG's factory team in 2010, excelling internationally with wins in the KF3 World Cup and WSK World Series. In 2013, at just 15, he made history by winning multiple European and World Championships in karting, becoming the youngest ever to win the KZ World Championship."/>
+              description="Hamilton began karting in 1993, quickly winning races and cadet class championships. At ten, he became the youngest driver to win the British cadet karting championship. That year, he met McLaren's Ron Dennis and expressed his ambition to race for McLaren, to which Dennis replied, Phone me in nine years, we'll sort something out then."/>
            
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Formula 3 ",
+    value: "formula 3",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham01.jpeg",
+              ]}
+              title="Junior Career"
+              description="Hamilton began his car racing career in the 2001 British Formula Renault Winter Series, finishing fifth. In 2002, he competed in the full Formula Renault UK campaign with Manor Motorsport, again finishing fifth. The next year, he won the championship, and debuted in the British Formula 3 Championship.In 2005, he joined ASM and dominated the Formula 3 Euro Series, winning 15 of 20 rounds and the Marlboro Masters of Formula 3. He also tested for McLaren at Silverstone in late 2004."/>
+
+      
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Formula 1",
+    value: "formula 1",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham02.jpeg",
+              ]}
+              title="Debut"
+              description="Hamilton debuted in Formula One with McLaren in 2007, partnering with two-time champion Fernando Alonso. He finished third in his debut race and set records, including most consecutive podium finishes from debut, joint most wins in a debut season, and most points in a debut season. He narrowly missed the World Championship, finishing one point behind Kimi Räikkönen."/>
+
+      
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Mclaren ",
+    value: "mclaren",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham03.jpeg",
+              ]}
+              title="World Champion"
+              description="In 2008, Hamilton won his first World Championship, securing five victories and ten podiums. He clinched the title in dramatic fashion at the Brazilian Grand Prix, overtaking Timo Glock on the final lap to finish fifth and beat Felipe Massa by one point, becoming the youngest champion at that time and the first British champion since 1996."/>
+
+      
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Misery",
+    value: "misery",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham04.jpeg",
+              ]}
+              title="Milestones"
+              description="From 2009 to 2012, Hamilton continued to achieve podiums and victories despite often driving a less competitive car. In 2010, he contended for the title but finished fourth. In 2011, he was outscored by teammate Jenson Button for the first time. In 2012, Hamilton finished fourth with four wins despite reliability issues. He announced his move to Mercedes for the 2013 season, replacing Michael Schumacher."/>
+
+      
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Mercedes",
+    value: "mercedes",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham05.jpeg",
+              ]}
+              title="2013-2016"
+              description="In 2013, Hamilton took a bold leap to Mercedes alongside childhood karting mate Nico Rosberg, despite the team's recent struggles. He notched one win and several podiums, clinching fourth in the championship. His triumph continued winning back to back Drivers Championships in 2014 and 2015 matching Ayrton Senna's three titles. The rivalry with Rosberg peaked during the United States Grand Prix, where Hamilton secured the championship with three races left. Despite a strong 2016 season with more poles and wins, Hamilton lost the championship to Rosberg by five points."/>
+      
+          </div>
+      
+      </div>
+    ),
+  },
+  {
+    title: "Maestro",
+    value: "meaestro",
+    content: (
+      <div className="md:mx-auto container  bg-white">
+     
+      
+        
+          <div className="md:mx-auto container  border-full border-2 ">
+            <BlogCard
+              imageUrls={[
+                "/drivers_champions/hamilton/ham06.jpeg",
+              ]}
+              title="2017-2020"
+              description="After Rosberg's retirement, Sebastian Vettel became Hamilton's main rival in 2017, with Hamilton securing his fourth World Drivers' Championship. He dominated with nine wins and set a new record for pole positions.
+
+In 2018, Hamilton and Vettel, both four-time champions, battled for a fifth title. Hamilton's strong second half of the season secured the championship in Mexico, setting a new points record for the season.
+
+Hamilton continued his dominance in 2019, clinching his sixth title with consistent performances against challengers like Bottas, Verstappen, and Leclerc.
+
+In 2020, despite the challenges of the COVID-19 pandemic, Hamilton won his seventh title, matching Michael Schumacher's record. He secured the championship with three races to spare.
+
+"/>
+      
           </div>
       
       </div>
