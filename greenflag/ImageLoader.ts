cecfa83859fca5796bde89edaf0248a5423db529
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-06-15 10:06:15 +1200

    Update ImageLoader.ts

--- greenflag/ImageLoader.ts
+++ greenflag/ImageLoader.ts
@@ -1,11 +1,11 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || 'Image Undefined'; // Fallback to empty string if not defined
+
 interface ImageLoaderProps {
-    src: string;
+  src: string;
 }
 
-const  ImageLoader = ({ src }: ImageLoaderProps): string => {
-
-    return `https://formula1heritage.cc/${src}`;
-}
+const ImageLoader = ({ src }: ImageLoaderProps): string => {
+  return `https://formula1heritage.cc${basePath}/${src}`;
+};
 
 export default ImageLoader;
-
