e578cf0e5a06c56c9e1b484eb46efd62f5545df0
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-06-15 10:50:47 +1200

    Update next.config.mjs

--- greenflag/next.config.mjs
+++ greenflag/next.config.mjs
@@ -1,49 +1,52 @@
 // next.config.js
 
-const path = require('path');
-const fs = require('fs');
+import path from 'path';
+import fs from 'fs/promises'; // Using fs/promises for async operations
 
 // Function to recursively find all directories under a given directory
-const getAllDirs = (baseDir) => {
+const getAllDirs = async (baseDir) => {
   const dirs = [];
-  
-  const findDirs = (currentDir) => {
-    const files = fs.readdirSync(currentDir, { withFileTypes: true });
-    files.forEach((file) => {
+
+  const findDirs = async (currentDir) => {
+    const files = await fs.readdir(currentDir, { withFileTypes: true });
+    for (const file of files) {
       if (file.isDirectory()) {
         const dirPath = path.join(currentDir, file.name);
         dirs.push(path.relative(baseDir, dirPath)); // Store relative path
-        findDirs(dirPath); // Recursive call for subdirectories
+        await findDirs(dirPath); // Recursive call for subdirectories
       }
-    });
+    }
   };
 
-  findDirs(baseDir);
+  await findDirs(baseDir);
   return dirs;
 };
 
 // Assuming your images are under the 'public' directory
 const publicDir = 'public';
-const imagePaths = getAllDirs(publicDir);
+let imagePaths = [];
+getAllDirs(publicDir).then((dirs) => {
+  imagePaths = dirs;
 
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  images: {
-    domains: ['formula1heritage.cc'],
-    path: imagePaths, // Array of all directories under 'public'
-    loader: 'custom',
-    loaderFile: './ImageLoader.ts',
-  },
-  experimental: {
-    appDir: true,
-  },
-  distDir: 'out',
-  output: 'export',
-};
+  const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      BASE_URL: process.env.BASE_URL,
+      NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    images: {
+      domains: ['formula1heritage.cc'],
+      path: imagePaths, // Array of all directories under 'public'
+      loader: 'custom',
+      loaderFile: './ImageLoader.ts',
+    },
+    experimental: {
+      appDir: true,
+    },
+    distDir: 'out',
+    output: 'export',
+  };
 
-module.exports = nextConfig;
+  module.exports = nextConfig;
+});
