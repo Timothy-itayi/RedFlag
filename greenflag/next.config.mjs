abb819ec2b0bfba47916abc6dae93b81c4c6e52c
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-06-15 10:18:07 +1200

    Update next.config.mjs

--- greenflag/next.config.mjs
+++ greenflag/next.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      BASE_URL: process.env.BASE_URL,
-    },
-    images: {
-        domains: ['formula1heritage.cc'],
-        path: `${basePath}/_next/images`,
-        loader: "custom",
-        loaderFile: "./ImageLoader.ts",
-        
-    },
-    experimental: {
-        appDir: true,
-    },
-    distDir: "out",
-    output: "export",
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    domains: ['formula1heritage.cc'], // Ensure your domain is listed here for Next.js to allow image loading
+    path: `${process.env.NEXT_PUBLIC_BASE_PATH}/tracks`, // Adjust this path based on your actual asset structure
+    loader: 'custom',
+    loaderFile: './ImageLoader.ts',
+  },
+  experimental: {
+    appDir: true,
+  },
+  distDir: 'out',
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
