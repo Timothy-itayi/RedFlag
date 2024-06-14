b0698bae565241ef0c7a08b5ef4d0ca930591542
Tim Itayi <timitayi@Timothys-Mac.local>
2024-06-14 20:17:40 +1200

    add &apos; and &qout; in about and manufacturer

--- greenflag/src/pages/manufacturers.tsx
+++ greenflag/src/pages/manufacturers.tsx
@@ -30,7 +30,7 @@ const Renualt = () => {
        <p className="font-bold text-4xl text-white">Renualt</p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-      Renault, a French automaker, has been in Formula One since 1977 as both a team owner and engine supplier. It introduced the turbo engine and won multiple titles with Williams and Benetton. After rebranding Benetton as Renault in 2000, it won championships in 2005 and 2006. Renault powered Red Bull's victories from 2010 to 2013 and rebranded its team as Alpine in 2021. Renault boasts two constructors' and drivers' titles as a team and 12 constructors' and 11 drivers' titles as an engine supplier.
+      Renault, a French automaker, has been in Formula One since 1977 as both a team owner and engine supplier. It introduced the turbo engine and won multiple titles with Williams and Benetton. After rebranding Benetton as Renault in 2000, it won championships in 2005 and 2006. Renault powered Red Bulls&apos; victories from 2010 to 2013 and rebranded its team as Alpine in 2021. Renault boasts two constructors' and drivers&apos; titles as a team and 12 constructors&apos; and 11 drivers&apos; titles as an engine supplier.
   </p>
     </div>
   );
@@ -42,7 +42,7 @@ const Mercedes = () => {
    <p className="font-bold text-4xl text-white">Mercedes</p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-2 max-w-lg text-neutral-200">
-      Mercedes-Benz, a German luxury automotive brand, active in Formula One since 1954, owns the Mercedes-AMG Petronas F1 Team. Known as the "Silver Arrows," they won titles in 1954 and 1955. Returning as an engine supplier in 1994, Mercedes found success with McLaren. Rebranding Brawn GP in 2010, Mercedes dominated from 2014 to 2021, winning seven Drivers' and eight Constructors' titles. Mercedes engines have over 200 wins, ranking second in F1 history with ten Constructors' and thirteen Drivers' Championships.
+      Mercedes-Benz, a German luxury automotive brand, active in Formula One since 1954, owns the Mercedes-AMG Petronas F1 Team. Known as the &quot;Silver Arrows,&quot; they won titles in 1954 and 1955. Returning as an engine supplier in 1994, Mercedes found success with McLaren. Rebranding Brawn GP in 2010, Mercedes dominated from 2014 to 2021, winning seven Drivers&apos; and eight Constructors&apos; titles. Mercedes engines have over 200 wins, ranking second in F1 history with ten Constructors&apos; and thirteen Drivers&apos; Championships.
 
 
       </p>
@@ -66,7 +66,7 @@ const Ferrari = () => {
       <p className="font-bold text-4xl text-white">Ferrari</p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-2 max-w-lg text-neutral-200">
-      Scuderia Ferrari, the racing arm of the luxury Italian automaker, is the oldest and most successful Formula One team, competing since 1950. Founded by Enzo Ferrari, it initially raced Alfa Romeo cars before building its own in 1947. Ferrari holds 16 Constructors' Championships and a record 15 Drivers' Championships. Michael Schumacher's five consecutive titles and 72 Grands Prix victories mark the team's most successful era. Their passionate fan base, known as the tifosi, and the Italian Grand Prix at Monza are iconic aspects of the team's heritage.
+      Scuderia Ferrari, the racing arm of the luxury Italian automaker, is the oldest and most successful Formula One team, competing since 1950. Founded by Enzo Ferrari, it initially raced Alfa Romeo cars before building its own in 1947. Ferrari holds 16 Constructors&apos; Championships and a record 15 Drivers&apos; Championships. Michael Schumacher's five consecutive titles and 72 Grands Prix victories mark the team&apos;s most successful era. Their passionate fan base, known as the tifosi, and the Italian Grand Prix at Monza are iconic aspects of the team&apos;s heritage.
       </p>
     </div>
   );
