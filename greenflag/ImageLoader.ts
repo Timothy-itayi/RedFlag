855518440f78669ad12f9253c877ead37fc1ec3e
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-06-15 11:34:02 +1200

    Update ImageLoader.ts

--- greenflag/ImageLoader.ts
+++ greenflag/ImageLoader.ts
@@ -1,15 +1,14 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 
 interface ImageLoaderProps {
   src: string;
 }
 
 const ImageLoader = ({ src }: ImageLoaderProps): string => {
-  
-  const imageUrl = `${basePath}/${src}`;
+  // const imageUrl = `${basePath}/${src}`;
+  const imageUrl = `/${src}`;
   console.log('Generated URL:', imageUrl); // Debugging line
   return imageUrl;
 };
-
 
 export default ImageLoader;
