2b6304ca55b8790be5ee50955ad14201c264d50b
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-06-17 14:32:48 +1200

    Update typings.d.ts

--- greenflag/src/apis/typings.d.ts
+++ greenflag/src/apis/typings.d.ts
@@ -3,7 +3,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
-      NEXT_PUBLIC_API_KEY: string; 
+      SECRET_API_KEY: string; 
       
     }
   }
